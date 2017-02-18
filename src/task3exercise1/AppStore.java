03c3a812f5cd1cec6b69592e77c4a19f85eeb1b7
Ressa Firmansyah Fatah <ressafatah@yahoo.co.id>
2017-02-18 21:10:16 +0700

    AppStore & Member
    1.0.0

--- src/task3exercise1/AppStore.java
+++ src/task3exercise1/AppStore.java
@@ -3,52 +3,76 @@ package task3exercise1;
 public class AppStore {
     
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     //
     //------------------- your code here-----------------------
+    
+    private Application[] appList = new Application[100];
+    private int totalApp;
     
     //---------------------------------------------------------
     
     // 2. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     //
     //------------------- your code here-----------------------
+
+    public Application[] getAppList() {
+        return appList;
+    }
+
+    public int getTotalApp() {
+        return totalApp;
+    }
     
     //---------------------------------------------------------
     
     
     // 3. Create method createNewApp( appName, appSize ) here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     // instantiate new Application object and insert it in array appList
     //
     //------------------- your code here-----------------------
+    
+    public void createNewApp(String appName, int appSize)
+    {
+        Application app = new Application(appName, appSize);
+        appList[totalApp] = app;
+        totalApp++;
+    }    
     
     //---------------------------------------------------------
     
     
     // 4. Create method getApp( id ) : Application here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     // returns Application object from array appList with array index = id
     //
     //------------------- your code here-----------------------
+    
+    public Application getApp(int id) 
+    {
+        return appList[id];
+    }
     
     //---------------------------------------------------------
         
     
     // 5. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     // @return returns String that contains the number of Application available
     // 
     // example : "There are 5 applications ready to install"
     // 
     public String toString() {
     //------------------- your code here-----------------------
-        return null;
+        String p = "There are " + totalApp + " application ready to install";
+        return p;
     
     //---------------------------------------------------------
     }
--- src/task3exercise2/Member.java
+++ src/task3exercise2/Member.java
@@ -3,34 +3,71 @@ package task3exercise2;
 public class Member {
 
     // 1. Declare your private attributes here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     //
     //------------------- your code here-----------------------
+    
+    private String name;
+    private String specialization;
+    private int projectWorked;
     
     //---------------------------------------------------------
     
     // 2. Declare your 2 Constructor here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     //
     //------------------- your code here-----------------------
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public Member(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+    }
     
     //---------------------------------------------------------
     
     
     // 3. Declare your Setter and Getter method here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     //
     //------------------- your code here-----------------------
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getProjectWorked() {
+        return projectWorked;
+    }
+
+    public void setProjectWorked(int projectWorked) {
+        this.projectWorked = projectWorked;
+    }
     
     //---------------------------------------------------------
     
     
     // 4. Create method toString() : String here:
-    // NIM : .........................., 
-    // NAMA : ........................., 
+    // NIM : 1301154105
+    // NAMA : Ressa Firmansyah Fatah
     //
     // returns String that contains member name, specialization, 
     // and number of project worked  
@@ -39,7 +76,9 @@ public class Member {
     //            worked on 5 project(s)"
     public String toString() {
     //------------------- your code here-----------------------
-        return null;
+    
+        String p = "Member " + name + " specialized in " + specialization + " and already worked on " + projectWorked + " projec(s)"; 
+        return p;
     
     //---------------------------------------------------------
     }
